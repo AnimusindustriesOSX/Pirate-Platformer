4b23ab044decbff7911b5b0eefb16a3626b26a0f
dkaratsin <dkaratsin@csd.auth.gr>
2024-07-20 18:17:08 +0300

    Added layerControl Script
    layerControl goes to every observeable gameobject except the ground

---
+++ Assets/Scripts/layerControl.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class layerControl : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    private void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        spriteRenderer.sortingOrder = (int)((transform.position.y*100)/100);
+    }
+}
