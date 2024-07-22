d0f77537030780e77a59e7af5b73ce4b7a928d8b
dkaratsin <dkaratsin@csd.auth.gr>
2024-07-23 01:29:55 +0300

    1

--- Assets/Scripts/layerControl.cs
+++ Assets/Scripts/layerControl.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 
-public class PlayerControl : MonoBehaviour
+public class layerControl : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     private void Start() {
