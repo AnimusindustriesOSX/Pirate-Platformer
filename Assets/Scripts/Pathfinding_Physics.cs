9c0588de5f73ceac36146036c810ead171721de0
sepehr <sepehrseddigi1379@gmail.com>
2024-07-25 17:01:14 +0330

    A*
    added Astar package

---
+++ Assets/Scripts/Pathfinding_Physics.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+using System.Timers;
+
+public class Pathfinding_Physics : MonoBehaviour
+{
+    
+    
+    [SerializeField] public float aggroTime;
+    
+    public bool isAIactive = true;
+    public Transform target; // Player target
+    public Transform homePosition; // Home position for the character
+    public float detectionRadius = 5f; // Radius to detect the player
+    public float idleTime = 2f; // Time to spend idling
+    public float wanderRadius = 10f; // Radius within which the character can wander
+
+    private Seeker_Module seeker_Module;
+    private AIPath aiPath;
+    private AIDestinationSetter aiDestinationSetter;
+    private float idleTimer;
+    private bool isIdle;
+    private Vector2 wanderCenter;
+    
+    private float secAggro;
+    
+    void Start()
+    {
+        seeker_Module = transform.GetComponent<Seeker_Module>();
+        aiPath = GetComponent<AIPath>();
+        aiDestinationSetter = GetComponent<AIDestinationSetter>();
+        aiDestinationSetter.target = homePosition; // Start with home position
+        idleTimer = idleTime;
+        isIdle = true;
+        wanderCenter = homePosition.position;
+    }
+
+    void Update()
+    {
+        if (target == null)
+            target = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        
+        
+        
+        if(isAIactive && seeker_Module){
+            float distanceToPlayer = Vector2.Distance(transform.position, target.position);
+            
+                
+                if(seeker_Module.isTargetDetected ){
+                    secAggro = aggroTime;
+                }
+                
+                if( secAggro > 0){
+                    secAggro -= Time.deltaTime;
+                    aiDestinationSetter.target = target;
+                    isIdle = false;
+                }
+                else{
+                    aiDestinationSetter.target = homePosition;
+                    isIdle = true;
+                    secAggro = 0;
+                }
+                }
+                /**
+                if (distanceToPlayer <= detectionRadius)
+                {
+                    // Player detected, follow the player
+                    aiDestinationSetter.target = target;
+                    isIdle = false;
+                }
+                else if (distanceToPlayer > detectionRadius && !isIdle)
+                {
+                    // Player lost, return to home position
+                    aiDestinationSetter.target = homePosition;
+                    isIdle = true;
+                }
+                **/
+            
+            // Handle idling
+            if (isIdle)
+            {
+                idleTimer -= Time.deltaTime;
+                if (idleTimer <= 0)
+                {
+                    // Perform idle actions (e.g., animations) and wander randomly
+                    Wander();
+                    idleTimer = idleTime; // Reset idle timer
+                }
+            }
+        }
+    
+
+    void Wander()
+    {
+        Vector2 randomDirection = Random.insideUnitCircle * wanderRadius;
+        Vector2 wanderTarget = wanderCenter + randomDirection;
+        aiDestinationSetter.target.position = wanderTarget;
+    }
+}
---
+++ Assets/Scripts/Seeker_Module.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Seeker_Module : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    [SerializeField] public Transform target;
+    [SerializeField] public float aggroRangeX;
+    [SerializeField] public float aggroRangeY;
+
+    [SerializeField] public LayerMask obstacleLayer;
+
+    [SerializeField] public bool isTargetDetected;
+
+    [SerializeField] float distanceToPlayerX;
+    [SerializeField] float distanceToPlayerY;
+
+    private bool isObstacleInWay;
+    private Vector2 start;
+    private Vector2 end;
+    private float distanceOffSet;
+
+    [Header("Debug")]
+    [SerializeField]  public bool isTargetOnRight;
+    
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").GetComponent<Transform>();
+    }
+
+    
+    void Update()
+    {
+        
+        if (target == null)
+            target = GameObject.FindWithTag("Player").GetComponent<Transform>();
+
+        //distanceToPlayerX = Vector2.Distance(transform.position, target.position);
+        if(transform.position.x - target.position.x > 0)isTargetOnRight = false;
+        else isTargetOnRight = true;
+        distanceToPlayerX = math.abs(transform.position.x - target.position.x);
+        distanceToPlayerY = math.abs(transform.position.y - target.position.y);
+        if (distanceToPlayerX <= aggroRangeX && distanceToPlayerY <= aggroRangeY)
+            {
+                
+                Color rayColor;
+                
+                
+                
+                start = transform.position;
+                end = target.transform.position;
+                distanceOffSet= (target.GetComponent<BoxCollider2D>().size.y)/2 - 0.1f;
+                Vector2 offsetDirection1 = new Vector2(end.x,end.y + distanceOffSet);
+                Vector2 offsetDirection2 = new Vector2(end.x,end.y - distanceOffSet);
+                Vector2 direction = (end- start).normalized;
+                Vector2 direction2 = ( offsetDirection1 - start).normalized;
+                Vector2 direction3 = ( offsetDirection2 - start).normalized;
+                float distance = Vector2.Distance(start, end);
+                float distance2 = Vector2.Distance(start, offsetDirection1);
+                float distance3 = Vector2.Distance(start, offsetDirection2);
+                // target in range, check if no colliders are in line of sight
+                isObstacleInWay = Physics2D.Raycast(start, direction, distance, obstacleLayer) && Physics2D.Raycast(start, direction2, distance2, obstacleLayer) && Physics2D.Raycast(start, direction3, distance3, obstacleLayer);
+
+                if (isObstacleInWay){
+                    rayColor = Color.red;
+                    isTargetDetected = false;
+                }
+                else{
+                    rayColor = Color.green;
+                    isTargetDetected = true;
+                }
+                //draw rays
+                Debug.DrawRay(start, direction * distance, rayColor);
+                Debug.DrawRay(start, direction2 * distance2, rayColor);
+                Debug.DrawRay(start, direction3 * distance3, rayColor);
+            
+            }
+        else{
+            isTargetDetected = false;
+        }
+    }
+}
